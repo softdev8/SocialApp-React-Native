a07145c739baa915c23529278293e2a4410147cd
Mobile Developer <oncemoreforyou@outlook.com>
2018-09-25 17:22:12 -0400

    fix eslint issue
    fixed

--- src/contactus.js
+++ src/contactus.js
@@ -10,31 +10,31 @@ import {
 import firebase from 'firebase';
 import NavigationBar from 'react-native-navigation-bar';
 export default class Chat extends Component{
-    render(){
-        return(
-            <Image source={require('./image/signback.png')} style={{flex:1,flexDirection:'column',justifyContent:'space-around'}}>
-            <NavigationBar title={'Chat'}
-                    height={44}
-                    titleColor={'#fff'}
-                    backgroundColor={'#d07b79'}
-                    leftButtonIcon={require('./image/back.png')}
-                    rightButtonIcon={require('./image/mainbutton.png')}
-                    onLeftButtonPress={()=>this.left()}
-                    onRightButtonPress={()=>this.right()}
-                    />
-            <Text style={{textAlign:'center',textAlignVertical:'center',fontSize:35,color:'#fff',alignSelf:'center'}}>My Social App</Text>
-            </Image>
-        );
-    }
-    
-    left(){
-       this.props.navigator.push({
-           name:'Home'
-       })
-   }
-   right(){
-       this.props.navigator.push({
-           name:'Home'
-       })
-   }
+  render(){
+    return(
+      <Image source={require('./image/signback.png')} style={{flex:1,flexDirection:'column',justifyContent:'space-around'}}>
+        <NavigationBar title={'Chat'}
+          height={44}
+          titleColor={'#fff'}
+          backgroundColor={'#d07b79'}
+          leftButtonIcon={require('./image/back.png')}
+          rightButtonIcon={require('./image/mainbutton.png')}
+          onLeftButtonPress={()=>this.left()}
+          onRightButtonPress={()=>this.right()}
+       />
+       <Text style={{textAlign:'center',textAlignVertical:'center',fontSize:35,color:'#fff',alignSelf:'center'}}>My Social App</Text>
+     </Image>
+   );
+ }
+ 
+ left(){
+   this.props.navigator.push({
+     name:'Home'
+   })
+ }
+ right(){
+   this.props.navigator.push({
+     name:'Home'
+   })
+ }
 }
